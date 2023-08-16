cbf924d5918769f917010c85a0065720c8dd0683
Reza Rezaeipour <rezarezaeipour@yahoo.com>
2023-08-16 11:34:56 +0330

    first Commit

---
+++ wiishy-app/src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Image from 'next/image'
+import { Button, ConfigProvider } from 'antd';
+import theme from '../../theme/themeConfig';
+
+export default function Home() {
+  return (
+    <>
+      <ConfigProvider theme={theme}>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+      </ConfigProvider>
+
+    </>
+
+  )
+}
