ae6f48f1e38662c0453d0c5d151bcb93df0ee501
arian <ariannargesi@gmail.com>
2023-08-19 10:50:02 +0330

    feat: header and sidebar

--- wiishy-app/src/app/layout.tsx
+++ wiishy-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import React from "react";
 import './globals.css';
+import Header from './components/header'
 import Navbar from './components/navbar';
 
 
@@ -20,7 +21,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
     <html>
       <body className={inter.className}>
         <React.StrictMode>
-
+          <Header/>
           {children}
           <Navbar/>
         </React.StrictMode>
