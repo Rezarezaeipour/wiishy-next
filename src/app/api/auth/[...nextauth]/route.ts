26d13e1991ae0855404ec636158c7887b65c4ed1
Reza Rezaeipour <rezarezaeipour@yahoo.com>
2023-09-22 23:03:00 +0330

    feat: Add LinkedIn OAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,35 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import LinkedInProvider from "next-auth/providers/linkedin";
 
 const handler = NextAuth({
   
+  debug:true,
   providers: [  
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    })
+    }),
+      LinkedInProvider({
+        clientId: process.env.LINKEDIN_CLIENT_ID as string,
+        clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
+        authorization: {
+          params: { scope: 'openid profile email' },
+        },
+        issuer: 'https://www.linkedin.com',
+        jwks_endpoint: 'https://www.linkedin.com/oauth/openid/jwks',
+      })
+    
   ],
-  // pages: {
-  //   // signIn: '/auth/signin',
-  //   signOut: '/auth/signout',
-  //   error: '/auth/error', // Error code passed in query string as ?error=
-  //   verifyRequest: '/auth/verify-request', // (used for check email message)
-  //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  // }
+  pages: {
+    signIn: '/login',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+  
 })
 
 export { handler as GET, handler as POST };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { signIn } from 'next-auth/react'
 export default function Login() {
   return (
     <div>
+      <button onClick={() => signIn('linkedin')}>Sign in with Linkedin</button>
       <button onClick={() => signIn('google')}>Sign in with Google</button>
     </div>
   )
