0caa37e2d6d3fd0c696e2e30300ae8bd55a45b86
Reza Rezaeipour <rezarezaeipour@yahoo.com>
2023-10-18 22:10:15 +0330

    Fix/ image positon and src in edit-profile segment - Adding myWrapperProfile

--- next.config.js
+++ next.config.js
@@ -2,7 +2,19 @@
 const nextConfig = {
     experimental: {
         serverActions: true
-    }
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'wiishy-backend.ir',
+            // port: '',
+            // pathname: '/account123/**',
+          },
+        ],
+      },
 }
+
+
 
 module.exports = nextConfig
