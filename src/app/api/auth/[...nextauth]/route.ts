f82e57dbd3fda3e762ca3f03c70bcd376c59c9d8
arian <ariannargesi@gmail.com>
2023-11-09 14:36:51 +0330

    little change

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,8 @@ const handler = NextAuth({
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID || '',
-      clientSecret: process.env.TWITTER_CLIENT_SECRET || ''
+      clientSecret: process.env.TWITTER_CLIENT_SECRET || '',
+      version: "2.0",
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
