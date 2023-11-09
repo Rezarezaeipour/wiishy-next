6529bdb3f3d0ebeff4764b4fe16af88712be561f
arian <ariannargesi@gmail.com>
2023-11-09 17:04:57 +0330

    put back id and secret for twitter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,8 +13,8 @@ const handler = NextAuth({
   debug: false,
   providers: [
     TwitterProvider({
-      clientId:'',
-      clientSecret:  '',
+      clientId: process.env.TWITTER_CLIENT_ID || '',
+      clientSecret: process.env.TWITTER_CLIENT_SECRET || '',
       version: "2.0",
     }),
     GoogleProvider({
