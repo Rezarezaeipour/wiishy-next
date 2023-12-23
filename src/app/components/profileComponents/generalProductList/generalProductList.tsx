d00621762756ea73d07f8bb133cb54f825e1ac7e
Reza Rezaeipour <rezarezaeipour@yahoo.com>
2023-12-23 23:47:41 +0330

    Feat/ Adding sharing page

---
+++ src/app/components/profileComponents/generalProductList/generalProductList.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { productListHandler } from "@/app/api-client/gifts";
+
+import { Product } from "@/types";
+import { Skeleton } from "antd";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import ProductCardNoFollow from "../../productComponents/productCardNoFollow/productCardNoFollow";
+
+function GeneralProductList(props: { userId: number }) {
+  const [gifts, setGifts] = useState<[Product]>();
+  useEffect(() => {
+    (async () => {
+      const ProductList = await productListHandler(props.userId);
+      setGifts(ProductList.gifts);
+    })();
+  }, [props.userId]);
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-0 mt-7 mb-10">
+        {gifts ? (
+          gifts.length > 0 ? (
+            gifts.map((Item, index) => {
+              return (
+                <ProductCardNoFollow
+                  key={"prd" + index}
+                  gift_name={Item.gift_name}
+                  gift_url={Item.gift_url}
+                  gift_price={Item.gift_price}
+                  desire_rate={Item.desire_rate}
+                  gift_id={Item.gift_id}
+                  gift_image_url={Item.gift_image_url}
+                  gift_like = {Item.gift_like}
+                />
+              );
+            })
+          ) : (
+            <p className="my-auto">Noting Added anything yet</p>
+          )
+        ) : (
+          <>
+            <Skeleton
+              paragraph={{ rows: 5 }}
+              active
+              className="mt-3 p-3 text-center"
+            />
+            <Skeleton
+              paragraph={{ rows: 5 }}
+              active
+              className="mt-3 p-3 text-center"
+            />
+             <Skeleton
+              paragraph={{ rows: 5 }}
+              active
+              className="mt-3 p-3 text-center"
+            />
+             <Skeleton
+              paragraph={{ rows: 5 }}
+              active
+              className="mt-3 p-3 text-center"
+            />
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default GeneralProductList;
