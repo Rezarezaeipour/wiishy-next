881645e690f65d75f4ad32056fb0f5f36179c635
Reza Rezaeipour <rezarezaeipour@yahoo.com>
2024-01-17 23:47:51 +0330

    Feat/ Gift Suggestion

--- src/app/api/gptengine/route.tsx
+++ src/app/api/gptengine/route.tsx
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest, res: NextResponse) {
   const API_URL = "https://api.openai.com/v1/chat/completions";
   const { prompt } = await req.json();
- 
+
   try {
     const response = await fetch(API_URL, {
       method: "POST",
@@ -22,17 +22,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
         model: "gpt-3.5-turbo-1106",
       }),
     });
-    console.log(await response);
 
     if (response.status == 200) {
       const data = await response.json();
-      console.log(data.choices[0].message.content);
-      
-     return NextResponse.json(data.choices[0].message.content)
+      return NextResponse.json(data.choices[0].message.content);
     }
   } catch (error) {
     console.log(error);
     console.error(error);
-    return NextResponse.json(error)
+    return NextResponse.json(error);
   }
 }
