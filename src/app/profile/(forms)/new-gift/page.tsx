63741509b3cb45a82eceda1177b313da0212aa78
Parastoo Mozhdeh <parastoo.mozhdeh@gmail.com>
2024-02-21 03:49:17 -0800

    Fixed the error
    - Fixed the closing tag </form> to </div>
    - Added a Category

--- src/app/profile/(forms)/new-gift/page.tsx
+++ src/app/profile/(forms)/new-gift/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, DotLoading, Dropdown, Form, Slider, Toast } from "antd-mobile";
+import { Button, DotLoading, Dropdown, Form, Slider, Toast, Selector } from "antd-mobile";
 import { useEffect, useRef, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import Image from "next/image";
@@ -13,6 +13,8 @@ import { HeartOutlined } from "@ant-design/icons";
 import { chatting } from "@/app/api-client/ai";
 import { Radio, Select, Space } from "antd";
 import { getCategoryList } from "@/app/api-client/category";
+import { options, fieldNamesOptions } from './options'
+
 
 function NewGift() {
   const { register, handleSubmit, setValue, reset } = useForm();
@@ -36,8 +38,8 @@ function NewGift() {
   const getUrl = async (url: string) => {
     url.length > 0
       ? (async () => {
-          setAiload(true);
-          const prompt = `I'll give you a link of an e-commerce wesite. Please give me the name, price, price unit and the main image address of the product 
+        setAiload(true);
+        const prompt = `I'll give you a link of an e-commerce wesite. Please give me the name, price, price unit and the main image address of the product 
            in a object with JSON format with these keys: name, price, price_unit, image_url. something like this : {product: { name: '', price : 00, price_unit:'',image_url:''}} .
            please dont add anyhting else, return just an json structure without json word. 
       for example, in this link:"https://www.digikala.com/product/dkp-8182784/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D9%86%D9%88%DA%A9%DB%8C%D8%A7-%D9%85%D8%AF%D9%84-g21-ta-1418-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-128-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-6-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/" the result is like this : 
@@ -47,23 +49,23 @@ function NewGift() {
       here is the link : ${url}
      `;
 
-          const response = await chatting(prompt);
-          const resObj = await JSON.parse(response);
-          const product = resObj.product;
+        const response = await chatting(prompt);
+        const resObj = await JSON.parse(response);
+        const product = resObj.product;
 
-          setValue("giftname", product.name);
-          setValue("giftprice", product.price);
+        setValue("giftname", product.name);
+        setValue("giftprice", product.price);
 
-          const price_unit = product.price_unit;
-          //  imageRef.current.value = product.price_unit;
-          setAiload(false);
-        })()
+        const price_unit = product.price_unit;
+        //  imageRef.current.value = product.price_unit;
+        setAiload(false);
+      })()
       : (() => {
-          Toast.show({
-            content: "Add a product link",
-            position: "bottom",
-          });
-        })();
+        Toast.show({
+          content: "Add a product link",
+          position: "bottom",
+        });
+      })();
   };
 
   /// Handle Submit
@@ -118,241 +120,306 @@ function NewGift() {
     2: 2,
     3: 3,
     4: 4,
-    5: 5,
+    5: 5
   };
 
   return (
-    <>
-      <div className="p-3 pb-20">
-        <h1 className="main-head">Add product</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Form.Item
-            className="font-normal text-xs mt-1"
-            style={{ backgroundColor: "transparent" }}
+    <div className="p-3 pb-20">
+      <h1 className="main-head">Add product</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Item
+          className="font-normal text-xs mt-1"
+          style={{ backgroundColor: "transparent" }}
+        >
+          <Radio.Group
+            defaultValue="0"
+            className="mt-4"
+            onChange={(val) => {
+              setIsproduct(val.target.value);
+              console.log(val.target.value);
+            }}
           >
-            <Radio.Group
-              defaultValue="0"
-              className="mt-4"
-              onChange={(val) => {
-                setIsproduct(val.target.value);
-                console.log(val.target.value);
-              }}
-            >
-              <Space direction="vertical">
-                <Radio value="1">It is my product</Radio>
-                <Radio value="0">It is my desire gift</Radio>
-              </Space>
-            </Radio.Group>
-            <p className="font-light text-xs text-gray-400 leading-4 mt-1">
-              If you are a producer and want to introduce your product choose
-              the first. Otherwise, if you are adding your desired gift, choose
-              the second.
-            </p>
-          </Form.Item>
-          <Form.Item
-            label="Product Image"
-            className="font-extrabold text-3xl mt-0"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <div className="flex justify-center pt-2 ">
-              <Image
-                src={image}
-                width={300}
-                height={250}
-                alt="wiishy user"
-                style={{
-                  width: "100%",
-                  height: "350px",
-                  objectFit: "cover",
-                  borderRadius: "10px",
-                  border: "solid thin silver",
-                }}
-              />
-            </div>
-            <div className="flex justify-center py-3 relative">
-              <AddCircleOutline
-                className="uploaGiftBtn"
-                onClick={() =>
-                  document.getElementById("input-image-gift")?.click()
-                }
-              />
-              <input
-                type="file"
-                name="file"
-                id="input-image-gift"
-                onChange={(e) => {
-                  setFile(e.target.files?.[0]);
-                  e.target.files?.[0] &&
-                    setImage(URL.createObjectURL(e.target.files?.[0]));
-                }}
-                style={{ display: "none" }}
-              />
-            </div>
-          </Form.Item>
-          {/* GIFT URL */}
-          <Form.Item
-            label="Product URL"
-            className="font-extrabold text-3xl "
-            style={{ backgroundColor: "transparent" }}
-          >
-            <input
-              autoComplete="off"
-              placeholder="https://amazon.com/xxx"
-              className="font-normal wiishy-input-text"
-              ref={urlRef}
-            />
-          </Form.Item>
-          {/* END URL */}
-          <div>
-            <div
-              className="float-right"
-              onClick={() => getUrl(urlRef.current.value)}
-            >
-              {aiload ? <DotLoading /> : <span>AI Fetch&nbsp;&gt;&gt;</span>}
-            </div>
-            <div className="clear-both"></div>
-          </div>
-          {/* GIFT NAME */}
-          {fetched}
+            <Space direction="vertical">
+              <Radio value="1">It is my product</Radio>
+              <Radio value="0">It is my desire gift</Radio>
+            </Space>
+          </Radio.Group>
+          <p className="font-light text-xs text-gray-400 leading-4 mt-1">
+            If you are a producer and want to introduce your product choose
+            the first. Otherwise, if you are adding your desired gift, choose
+            the second.
+          </p>
+        </Form.Item>
 
-          <Form.Item
-            label="Product name"
-            className="font-extrabold text-3xl "
-            style={{ backgroundColor: "transparent" }}
-          >
-            <input
-              autoComplete="off"
-              placeholder="Electric bicycle"
-              className="font-normal wiishy-input-text"
-              {...register("giftname", { required: true, maxLength: 200 })}
-            />
-          </Form.Item>
+      </form>
+      <Form.Item
+        label="Product Image"
+        className="font-extrabold text-3xl mt-0"
+        style={{ backgroundColor: "transparent" }}
+      >
+        <div className="flex justify-center pt-2 ">
+          <Image
+            src={image}
+            width={300}
+            height={250}
+            alt="wiishy user"
+            style={{
+              width: "100%",
+              height: "350px",
+              objectFit: "cover",
+              borderRadius: "10px",
+              border: "solid thin silver",
+            }}
+          />
+        </div>
 
-          {/* END GIFT NAME */}
-
-          {/* GIFT PRICE */}
-          <Form.Item
-            label="Product Price"
-            style={{ backgroundColor: "transparent" }}
-          ></Form.Item>
-          {/* END GIFT PRICE */}
-
-          <div className="flex">
-            <input
-              autoComplete="off"
-              type="number"
-              placeholder="250"
-              className="font-normal  wiishy-input-text py-3 basis-3/4"
-              maxLength={12}
-              min={1}
-              style={{
-                height: "45px",
-                borderRight: "none",
-                borderRadius: "5px 0px 0px 5px",
-              }}
-              {...register("giftprice", { required: true, maxLength: 12 })}
-            />
-            <Select
-              labelInValue
-              defaultValue={{ value: `${priceunit}` }}
-              className="basis-1/4 currency-list"
-              onChange={(v) => setPriceunit(v.value)}
-              options={[
-                {
-                  value: "1",
-                  label: "$",
-                },
-                {
-                  value: "2",
-                  label: "€",
-                },
-                {
-                  value: "3",
-                  label: "£",
-                },
-                {
-                  value: "4",
-                  label: "IRR",
-                },
-              ]}
-            />
-            {/* <Dropdown
-              className=" "
-              style={{
-                border: "solid thin silver",
-                borderRadius: "0px 5px 5px 0px",
-                height: "45px",
-              }}
-              closeOnMaskClick={true}
-            >
-              <Dropdown.Item key="sorter" title="Unit">
-                <div style={{ padding: 12 }}>
-                  <p onClick={() => setPriceunit(1)}>$</p>
-                  <br />
-                  <p onClick={() => setPriceunit(2)}>€</p>
-                  <br />
-                  <p onClick={() => setPriceunit(3)}>£</p>
-                  <br />
-                  <p onClick={() => setPriceunit(4)}>IRR</p>
-                </div>
-              </Dropdown.Item>
-            </Dropdown> */}
-          </div>
-
-          {/* GIFT DESIRE */}
-          <Form.Item
-            label="How much do you love it?"
-            className="font-extrabold text-3xl"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <div className="my-4">
-              <Slider
-                marks={marks}
-                ticks
-                onAfterChange={(value) => setDesire(value)}
-                defaultValue={3}
-                max={5}
-                icon={
-                  <HeartOutlined className="mx-auto flex justify-center items-center h-full w-full text-[15px]" />
-                }
-              />
-            </div>
-          </Form.Item>
-          {/* Endi GIFT DESIRE */}
-          {/* GIFT DESCRIPTION */}
-          <Form.Item
-            label="Product description"
-            className="font-extrabold text-3xl"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <textarea
-              autoComplete="false"
-              placeholder="Write atleast three lines about the product"
-              maxLength={100}
-              rows={3}
-              className="font-normal wiishy-input-text"
-              {...register("giftdescription")}
-            />
-          </Form.Item>
-          {/* END GIFT DESCRIPTION */}
-
-          {/* SUBMIT BUTTON */}
-
-          <div className="pb-5 px-2 mt-1 fixed bottom-0 left-0 w-full z-10">
-            <Button
-              loading={loading}
-              type="submit"
-              className="btn btn-regular btn-big-style w-full m-1"
-              style={{ fontSize: "14px" }}
-            >
-              Save product
-            </Button>
-
-            {/* END SUBMIT BUTTON */}
-          </div>
-        </form>
+        <div className="flex justify-center py-3 relative">
+          <AddCircleOutline
+            className="uploaGiftBtn"
+            onClick={() =>
+              document.getElementById("input-image-gift")?.click()
+            }
+          />
+          <input
+            type="file"
+            name="file"
+            id="input-image-gift"
+            onChange={(e) => {
+              setFile(e.target.files?.[0]);
+              e.target.files?.[0] &&
+                setImage(URL.createObjectURL(e.target.files?.[0]));
+            }}
+            style={{ display: "none" }}
+          />
+        </div>
+      </Form.Item>
+      {/* GIFT URL */}
+      <Form.Item
+        label="Product URL"
+        className="font-extrabold text-3xl "
+        style={{ backgroundColor: "transparent" }}
+      >
+        <input
+          autoComplete="off"
+          placeholder="https://amazon.com/xxx"
+          className="font-normal wiishy-input-text"
+          ref={urlRef}
+        />
+      </Form.Item>
+      {/* END URL */}
+      <div>
+        <div
+          className="float-right"
+          onClick={() => getUrl(urlRef.current.value)}
+        >
+          {aiload ? <DotLoading /> : <span>AI Fetch&nbsp;&gt;&gt;</span>}
+        </div>
+        <div className="clear-both"></div>
       </div>
-    </>
+      {/* GIFT NAME */}
+      {fetched}
+
+      <Form.Item
+        label="Product name"
+        className="font-extrabold text-3xl "
+        style={{ backgroundColor: "transparent" }}
+      >
+        <input
+          autoComplete="off"
+          placeholder="Electric bicycle"
+          className="font-normal wiishy-input-text"
+          {...register("giftname", { required: true, maxLength: 200 })}
+        />
+      </Form.Item>
+
+      {/* END GIFT NAME */}
+
+      {/* GIFT PRICE */}
+      <Form.Item
+        label="Product Price"
+        style={{ backgroundColor: "transparent" }}
+      ></Form.Item>
+      {/* END GIFT PRICE */}
+
+
+      <div className="flex">
+        <input
+          autoComplete="off"
+          type="number"
+          placeholder="250"
+          className="font-normal  wiishy-input-text py-3 basis-3/4"
+          maxLength={12}
+          min={1}
+          style={{
+            height: "45px",
+            borderRight: "none",
+            borderRadius: "5px 0px 0px 5px",
+          }}
+          {...register("giftprice", { required: true, maxLength: 12 })}
+        />
+        <Select
+          labelInValue
+          defaultValue={{ value: `${priceunit}` }}
+          className="basis-1/4 currency-list"
+          onChange={(v) => setPriceunit(v.value)}
+          options={[
+            {
+              value: "1",
+              label: "$",
+            },
+            {
+              value: "2",
+              label: "€",
+            },
+            {
+              value: "3",
+              label: "£",
+            },
+            {
+              value: "4",
+              label: "IRR",
+            },
+          ]}
+        />
+        {/* <Dropdown
+            className=" "
+            style={{
+              border: "solid thin silver",
+              borderRadius: "0px 5px 5px 0px",
+              height: "45px",
+            }}
+            closeOnMaskClick={true}
+          >
+            <Dropdown.Item key="sorter" title="Unit">
+              <div style={{ padding: 12 }}>
+                <p onClick={() => setPriceunit(1)}>$</p>
+                <br />
+                <p onClick={() => setPriceunit(2)}>€</p>
+                <br />
+                <p onClick={() => setPriceunit(3)}>£</p>
+                <br />
+                <p onClick={() => setPriceunit(4)}>IRR</p>
+              </div>
+            </Dropdown.Item>
+          </Dropdown> */}
+      </div>
+
+      {/* Add Cattegory  */}
+
+      <div
+        title="Category"
+        className="font-extrabold text-3xl"
+        style={{ backgroundColor: "transparent" }}
+      >
+      
+
+        <Selector
+          style={{
+            "--border-radius": "100px",
+            "--border": "solid transparent 1px",
+            "--checked-border": "none",
+            "--padding": "8px 24px",
+            "--color": "#f5f5f5",
+            fontSize: "13px",
+            fontWeight: "normal",
+          }}
+          showCheckMark={false}
+          options={[
+            {
+              label: "Hobby",
+              value: "1",
+            },
+            {
+              label: "Tech",
+              value: "2",
+            },
+            {
+              label: "Tech1",
+              value: "3",
+            },
+            {
+              label: "Tech2",
+              value: "4",
+            },
+            {
+              label: "Tech3",
+              value: "5",
+            },
+            {
+              label: "Tech4",
+              value: "6",
+            },
+
+          ]}
+
+          columns={4}
+          defaultValue={['2', '3', '4']}
+          multiple
+
+        />
+      </div>
+
+      {/* End Category */}
+
+      {/* Subcategory */}
+
+      <div title="Subcategory">
+
+      </div>
+
+
+      {/* GIFT DESIRE */}
+      <Form.Item
+        label="How much do you love it?"
+        className="font-extrabold text-3xl"
+        style={{ backgroundColor: "transparent" }}
+      >
+        <div className="my-4">
+          <Slider
+            marks={marks}
+            ticks
+            onAfterChange={(value) => setDesire(value)}
+            defaultValue={3}
+            max={5}
+            icon={
+              <HeartOutlined className="mx-auto flex justify-center items-center h-full w-full text-[15px]" />
+            }
+          />
+        </div>
+      </Form.Item>
+      {/* Endi GIFT DESIRE */}
+      {/* GIFT DESCRIPTION */}
+      <Form.Item
+        label="Product description"
+        className="font-extrabold text-3xl"
+        style={{ backgroundColor: "transparent" }}
+      >
+        <textarea
+          autoComplete="false"
+          placeholder="Write atleast three lines about the product"
+          maxLength={100}
+          rows={3}
+          className="font-normal wiishy-input-text"
+          {...register("giftdescription")}
+        />
+      </Form.Item>
+      {/* END GIFT DESCRIPTION */}
+
+      {/* SUBMIT BUTTON */}
+
+      <div className="pb-5 px-2 mt-1 fixed bottom-0 left-0 w-full z-10">
+        <Button
+          loading={loading}
+          type="submit"
+          className="btn btn-regular btn-big-style w-full m-1"
+          style={{ fontSize: "14px" }}
+        >
+          Save product
+        </Button>
+
+        {/* END SUBMIT BUTTON */}
+      </div>
+    </div >
   );
 }
 
